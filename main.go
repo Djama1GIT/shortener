65b4cd0f330946ae85b5bdeb67feb62838d433be
Djama1GIT <mail@dj.ama1.ru>
2023-08-07 03:11:06 +0300

    front-end

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func agreement(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/agreement.html")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fs := http.FileServer(http.Dir("templates/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/agreement", agreement)
+	http.ListenAndServe(":8081", nil)
 }
